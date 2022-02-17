ca0260b8cbdcbbdbd529f1e538563cbc312039ec
liangjiedong <liangjiedong@yingzi.com>
2022-02-18 00:33:48 +0800

    from proxy

--- src/main/java/dustni/tutorial/mydemo/controller/HelloController.java
+++ src/main/java/dustni/tutorial/mydemo/controller/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
     @ResponseBody
     @RequestMapping("/")
     public String hello() {
-        return "Hello Docker,change6";
+        return "Hello Docker,change,from proxy";
     }
 }
